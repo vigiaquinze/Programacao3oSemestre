866da435fe5b66fdf270ea3f76e3dfbf6d216974
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-02-02 15:11:56 -0300

    2.2 15h11

---
+++ AtividadesRevisao/Elevador/Elevador3oTermo/src/Elevador.java
@@ -0,0 +1,19 @@
+public class Elevador {
+    //atributos
+    private int andarAtual;
+
+    //métodos
+    public Elevador() {
+        this.andarAtual=0;
+    }
+    //getters and setters
+
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+
+    public void setAndarAtual(int andarAtual) {
+        this.andarAtual = andarAtual;
+    }
+    
+}
---
+++ AtividadesRevisao/Elevador/Elevador3oTermo/src/JanelaElevadores.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class JanelaElevadores extends JFrame{
+    //atributos
+    Elevador elevador1 = new Elevador();
+    Elevador elevador2 = new Elevador();
+    //construtor
+    public JanelaElevadores() {
+        super("Elevadores em Java");
+    }
+}
