370aebf2ccb29972b9b4f80655b1b383f16c0a64
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-03-04 16:03:57 -0300

    4.3

---
+++ CursoSpringBoot/helloworld/src/test/java/webapp/helloworld/IndexController.java
@@ -0,0 +1,33 @@
+package webapp.helloworld;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/** @author Rolfi Luz - Senai * */
+@Controller
+public class IndexController {
+
+@RequestMapping(value = "/", method = RequestMethod.GET)
+public ModelAndView abrirIndex() {
+ModelAndView mv = new ModelAndView("index");
+
+String mensagem = "Olá, seja bem-vindo!";
+mv.addObject("msg", mensagem);
+
+return mv;
+}
+
+@RequestMapping(value = "/", method = RequestMethod.POST)
+public ModelAndView buscarIndex(@RequestParam("buscar") String buscar) {
+ModelAndView mv = new ModelAndView("index");
+
+String mensagem = "Resultado da Busca !";
+mv.addObject("msg", mensagem);
+mv.addObject("buscar", buscar);
+
+return mv;
+}
+
+}
