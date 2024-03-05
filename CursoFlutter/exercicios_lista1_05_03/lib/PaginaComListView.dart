b39b88fd327c9c2d61cc1be8d2f3e9e8328b4234
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-03-05 16:13:56 -0300

    5.3

--- CursoFlutter/exercicios_lista1_05_03/lib/PaginaComListView.dart
+++ CursoFlutter/exercicios_lista1_05_03/lib/PaginaComListView.dart
@@ -1,39 +1,72 @@
-// Importa o pacote de material Flutter
 import 'package:flutter/material.dart';
 
-// Função principal que inicializa a aplicação Flutter
 void main() {
-// Inicia a execução da aplicação com o widget MyApp
   runApp(MyApp());
 }
 
-// Definindo a classe MyApp que herda de StatelessWidget
 class MyApp extends StatelessWidget {
-// Método build, obrigatório em StatelessWidget, retorna a árvore de widgets da aplicação
   @override
   Widget build(BuildContext context) {
-    // Retorna um MaterialApp, que é o widget raiz da aplicação Flutter
     return MaterialApp(
-// Configura a tela inicial como um Scaffold
-        home: Scaffold(
-// Barra superior da tela (AppBar)
-      appBar: AppBar(
-// Título da barra superior
-        title: Text('Página com ListView'),
-      ),
-      body: Column(
-        children: [
-          ListView(
-            children: [
-              ListTile(
-                title: Text('Nome'),
-                subtitle: Text('Luise Inácie Lule de Silve'),
-                textColor: Colors.black87,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Página com ListView'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text(
+                      'Nome',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    subtitle: Text('Luise Inácie Lule de Silve'),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Data de Nascimento',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    subtitle: Text('27/10/1945'),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Filhos',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    subtitle: Text(
+                        'Lurian Cordeiro Lula da Silva, Sandro Luís, Marcos Cláudio Lula da Silva, Luís Cláudio, Fábio Luís Lula da Silva'),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Partido',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    subtitle: Text('Partido dos Trabalhadores'),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Cônjuge',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    subtitle: Text('Janja Lula da Silva'),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Mandatos presidenciais',
+                      style: TextStyle(fontWeight: FontWeight.w900),
+                    ),
+                    subtitle: Text(
+                        '1 de janeiro de 2023 –, 1 de janeiro de 2003 – 1 de janeiro de 2011'),
+                  ),
+                ],
               ),
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
