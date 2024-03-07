4b7c50d5c9f6c3fc4683a515cbec9e6215c3b5a5
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-03-07 13:57:23 -0300

    4.3

---
+++ CursoSpringBoot/newletterjdbc/src/main/java/webapp/newletterjdbc/NewletterjdbcApplication.java
@@ -0,0 +1,13 @@
+package webapp.newletterjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewletterjdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewletterjdbcApplication.class, args);
+	}
+
+}
