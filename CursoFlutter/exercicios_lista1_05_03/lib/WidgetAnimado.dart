863cc730df052b174f15deea36511420a4c57347
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-03-12 16:19:45 -0300

    12.3

---
+++ CursoFlutter/exercicios_lista1_05_03/lib/WidgetAnimado.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+// Função principal que inicializa a aplicação Flutter
+void main() {
+// Inicia a execução da aplicação com o widget MyApp
+  runApp(MyApp());
+}
+
+// Definindo a classe MyApp que desta vez herda de StatefulWidget, para desta vez ter um estado que será usado no createState()
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Define as propriedades padrão do objeto
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
+  Widget build(BuildContext context) {
+    AnimatedContainer(
+      // Usa as propriedades armazenadas no estado da classe
+      width: _width,
+      height: _height,
+      decoration: BoxDecoration(
+        color: _color,
+        borderRadius: _borderRadius,
+      ),
+      // Definindo o quanto a animação irá durar.
+      duration: const Duration(seconds: 1),
+      // Adicionando um método de curva para a animação, para ela parecer mais suave.
+      curve: Curves.fastOutSlowIn,
+    );
+    return MaterialApp(
+// Configura a tela inicial como um Scaffold
+      home: Scaffold(
+        appBar: AppBar(
+// Título da barra superior
+          title: Text('Widget Animado'),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
+            duration: const Duration(seconds: 1),
+            curve: Curves.fastOutSlowIn,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          // Quando o usuário aperta o botão de 'play'
+          onPressed: () {
+            // Usando setState() para rearmazenar as propriedades do widget.
+            setState(() {
+              // Cria um gerador aleatório.
+              final random = Random();
+
+              // Gera largura e altura aleatoriamente.
+              _width = random.nextInt(300).toDouble();
+              _height = random.nextInt(300).toDouble();
+
+              // Gera uma cor aleatória usando cada cor do espectro RGB em valores aleatórios.
+              _color = Color.fromRGBO(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+                1,
+              );
+
+              // Gera um "border radius" aleatório.
+              _borderRadius =
+                  BorderRadius.circular(random.nextInt(100).toDouble());
+            });
+          },
+          child: const Icon(Icons.play_arrow),
+        ),
+      ),
+    );
+  }
+}
