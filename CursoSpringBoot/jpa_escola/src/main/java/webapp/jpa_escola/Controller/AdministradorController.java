cbee395f77d256e90bfc84b4732413098e69f6b8
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-03-26 17:07:21 -0300

    26.3

--- CursoSpringBoot/jpa_escola/src/main/java/webapp/jpa_escola/Controller/AdministradorController.java
+++ CursoSpringBoot/jpa_escola/src/main/java/webapp/jpa_escola/Controller/AdministradorController.java
@@ -7,19 +7,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import webapp.jpa_escola.Model.Administrador;
 import webapp.jpa_escola.Repository.AdministradorRepository;
-
+import webapp.jpa_escola.Repository.PreCadAdmRepository;
 
 @Controller
 public class AdministradorController {
     @Autowired
     private AdministradorRepository ar;
 
-    @PostMapping("cadastro-adm")
-    public String postCadastroAdm(Administrador adm) {
-        ar.save(adm);
-        //Enviar uma mensagem de cadastro realizada com sucesso
-        System.out.println("Cadastro realizado com sucesso.");
+    @Autowired
+    private PreCadAdmRepository pcar;
+
+    String cpfVerificacao = pcar.findByCpf(adm.getCpf()).getCpf();
+    
+        if (cpfVerificacao.equals(adm.getCpf())) {
+            ar.save(adm);
+            // Enviar uma mensagem de cadastro realizada com sucesso
+            System.out.println("Cadastro realizado com sucesso.");
+        }
         return "index";
     }
-    
+
 }
