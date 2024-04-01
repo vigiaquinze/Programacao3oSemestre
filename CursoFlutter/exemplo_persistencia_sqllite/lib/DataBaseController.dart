9624427c153f9cab65c9370fd680e040ec0f8957
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-04-01 14:33:12 -0300

    01.04

--- CursoFlutter/exemplo_persistencia_sqllite/lib/DataBaseController.dart
+++ CursoFlutter/exemplo_persistencia_sqllite/lib/DataBaseController.dart
@@ -18,4 +18,66 @@ class DatabaseHelper {
       version: 1,
     );
   }
+   // Método para criar um novo contato no banco de dados
+  Future<void> create(ContactModel model) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.insert(
+          TABLE_NAME, model.toMap()); // Insere o contato no banco de dados
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
+
+  // Método para obter todos os contatos do banco de dados
+  Future<List<ContactModel>> getContacts() async {
+    try {
+      final Database db = await _getDatabase();
+      final List<Map<String, dynamic>> maps =
+          await db.query(TABLE_NAME); // Consulta todos os contatos na tabela
+
+      return List.generate(
+        maps.length,
+        (i) {
+          return ContactModel.fromMap(maps[
+              i]); // Converte os resultados da consulta para objetos ContactModel
+        },
+      );
+    } catch (ex) {
+      print(ex);
+      return [];
+    }
+  }
+
+  // Método para atualizar um contato no banco de dados
+  Future<void> update(ContactModel model) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.update(
+        TABLE_NAME,
+        model.toMap(),
+        where: "id = ?", // Condição para atualizar o contato com base no ID
+        whereArgs: [model.id],
+      );
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
+
+  // Método para excluir um contato do banco de dados
+  Future<void> delete(int id) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.delete(
+        TABLE_NAME,
+        where: "id = ?", // Condição para excluir o contato com base no ID
+        whereArgs: [id],
+      );
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
 }
