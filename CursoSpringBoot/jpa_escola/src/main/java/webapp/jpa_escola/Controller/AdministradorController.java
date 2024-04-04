5428a9760ea00160619a30f0f72bc90e0f10c78b
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-04-04 17:00:29 -0300

    4.4

--- CursoSpringBoot/jpa_escola/src/main/java/webapp/jpa_escola/Controller/AdministradorController.java
+++ CursoSpringBoot/jpa_escola/src/main/java/webapp/jpa_escola/Controller/AdministradorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import webapp.jpa_escola.Model.Administrador;
 import webapp.jpa_escola.Repository.AdministradorRepository;
@@ -14,18 +15,18 @@ import webapp.jpa_escola.Repository.PreCadAdmRepository;
 @Controller
 public class AdministradorController {
     @Autowired
-    private AdministradorRepository ar;
+    private AdministradorRepository admr;
 
     @Autowired
     private PreCadAdmRepository pcar;
 
     boolean acessoAdm = false;
     
-    @PostMapping("cadastrar-adm")
+    @PostMapping("cadastro-adm")
     public String cadastrarAdmBD(Administrador adm) {
         boolean verificaCpf = pcar.existsById(adm.getCpf());
         if (verificaCpf) {
-            ar.save(adm);
+            admr.save(adm);
             System.out.println("Cadastro Realizado com Sucesso");
         } else {
             System.out.println("Falha ao Cadastrar");
@@ -45,21 +46,28 @@ public class AdministradorController {
     }
 
         @PostMapping("acesso-adm")
-    public String acessoAdm(@RequestParam String cpf,
+    public ModelAndView acessoAdm(@RequestParam String cpf,
             @RequestParam String senha) {
+        ModelAndView mv = new ModelAndView();
         try {
-            boolean verificaCpf = ar.existsById(cpf);
-            boolean verificaSenha = ar.findByCpf(cpf).getSenha().equals(senha);
+            boolean verificaCpf = admr.existsById(cpf);
+            boolean verificaSenha = admr.findByCpf(cpf).getSenha().equals(senha);
             String url = "";
             if (verificaCpf && verificaSenha) {
                 acessoAdm = true;
                 url = "redirect:/interna-adm";
+                mv.addObject("msg", "Logado com sucesso!");
+                mv.setViewName(url);
             } else {
                 url = "redirect:/login-adm";
+                mv.addObject("msg", "Erro! Acesso negado ou senha incorreta.");
+                mv.setViewName(url);
             }
-            return url;
+            return mv;
         } catch (Exception e) {
-            return "redirect:/login-adm";
+            mv.addObject("msg", "Erro no login.");
+            mv.setViewName("redirect:/login-adm");
+            return mv;
         }
 
     }
