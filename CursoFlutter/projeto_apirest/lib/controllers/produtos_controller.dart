2f2bc7f6905d5b94b98b19f6ffdd48d102a1fdaf
JoaovlLima <jl949615@gmail.com>
2024-06-04 13:17:23 -0300

    4.6

--- CursoFlutter/projeto_apirest/lib/controllers/produtos_controller.dart
+++ CursoFlutter/projeto_apirest/lib/controllers/produtos_controller.dart
@@ -1,29 +1,29 @@
 import 'dart:convert';
 
-import 'package:projeto_apirest/models/produto.dart';
+import '../models/produto.dart';
 import 'package:http/http.dart' as http;
 
 class ProdutosController {
   List<Produto> _listProdutos = [];
-
+  
   List<Produto> get listProdutos => _listProdutos;
-
-  //getProdutosFromJson
+  
+  //getPRodutosFromJson http
   Future<List<Produto>> getProdutosFromJson() async {
-    final response =
-        await http.get(Uri.parse('http://10.109.207.164:3000/produtos'));
+    final response = await http.get(Uri.parse('http://10.109.194.66:3000/produto'));
     if (response.statusCode == 200) {
-      List<Map<String, dynamic>> result = await json.decode(response.body);
+      List<dynamic> result = await json.decode(response.body);
       _listProdutos = result.map((prod) => Produto.fromJson(prod)).toList();
       return _listProdutos;
     } else {
       throw Exception('Failed to load Produtos');
     }
   }
-    //postProdutos toJson http
+  //postProdutos toJson http
   Future<String> postProdutosToJson(Produto produto) async {
-    final response = await http.post(Uri.parse('http://10.109.207.164:3000/produtos'),
-        body: json.encode(produto.toJson()));
+    final response = await http.post(Uri.parse('http://10.109.194.66:3000/produto'),
+        body: json.encode(produto.toJson()),
+        headers: {'Content-Type': 'application/json'});
     if (response.statusCode == 201) {
       return response.body;
     } else {
--- CursoFlutter/projeto_apirest/lib/screens/cadastro_produto_screen.dart
+++ CursoFlutter/projeto_apirest/lib/screens/cadastro_produto_screen.dart
@@ -1,30 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_apirest/controller/produtos_controller.dart';
-import 'package:projeto_apirest/models/produto.dart';
+import '../controllers/produtos_controller.dart';
+import '../models/produto.dart';
 
-class CadastroProdutoScreenState extends StatefulWidget {
-  const CadastroProdutoScreenState({super.key});
+class CadastroProdutoScreen extends StatefulWidget {
+  const CadastroProdutoScreen({super.key});
 
   @override
-  State<CadastroProdutoScreenState> createState() => _CadastroProdutoScreenStateState();
+  State<CadastroProdutoScreen> createState() => _CadastroProdutoScreenState();
 }
 
-class _CadastroProdutoScreenStateState extends State<CadastroProdutoScreenState> {
+class _CadastroProdutoScreenState extends State<CadastroProdutoScreen> {
   final ProdutosController _controller = ProdutosController();
   final TextEditingController _nomeProduto = TextEditingController();
   final TextEditingController _precoProduto = TextEditingController();
   final TextEditingController _idProduto = TextEditingController();
 
+  //future CadatroPRoduto
   Future<void> _cadastrarProduto() async {
     try {
-      Produto produto = Produto (id: _idProduto.text, nome: _nomeProduto.text, preco: double.parse(_precoProduto.text)) await _controller.postProdutosToJson(produto);
+      Produto produto =  Produto(
+        id: _idProduto.text, 
+        nome: _nomeProduto.text, 
+        preco: double.parse(_precoProduto.text));
+      await _controller.postProdutosToJson(produto);
     } catch (e) {
-      
+      print(e);
     }
+    
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de Produtos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _nomeProduto,
+              decoration: InputDecoration(
+                labelText: 'Nome do Produto',
+              ),
+            ),
+            TextField(
+              controller: _precoProduto,
+              decoration: InputDecoration(
+                labelText: 'Preço do Produto',
+              ),
+            ),
+            TextField(
+              controller: _idProduto,
+              decoration: InputDecoration(
+                labelText: 'ID do Produto',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _cadastrarProduto,
+              child: Text('Cadastrar'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
