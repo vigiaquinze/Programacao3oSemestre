589c742824d0d26cee621dc00c8692640c4c309f
JoaovlLima <jl949615@gmail.com>
2024-06-11 16:25:16 -0300

    11.6

--- CursoFlutter/exemplo2_music_player/lib/screens/home_screen.dart
+++ CursoFlutter/exemplo2_music_player/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
-import 'package:exemplo2_music_player/screens/music_player_screen.dart';
 import 'package:exemplo2_music_player/services/music_service.dart';
 import 'package:flutter/material.dart';
+
+import 'music_player_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,54 +11,56 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final MusicService _musicService = new MusicService();
+  final MusicService _service = MusicService();
 
-  Future<void> _getList() async {
+  Future<void> _getList() async{
     try {
-      await _musicService.fetchList();
+      await _service.fetchList();
     } catch (e) {
       print(e.toString());
     }
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(12),
-          child: Center(
-            child: Column(
-              children: [
-                Expanded(child: FutureBuilder(future: _getList(), builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  } else if (_musicService.list.isEmpty) {
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: 
+      Padding(
+        padding: EdgeInsets.all(8),
+        child: Center(
+          child:Column(
+            children: [
+              Expanded(
+                child: FutureBuilder(
+                  future: _getList(), 
+                  builder: (context,snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return Center(child: CircularProgressIndicator());
+                    }else if(_service.list.isEmpty){
                       return Center(child: Text('Não há músicas cadastradas'));
-                  } else {
-                        return ListView.builder(
-                        itemCount: _musicService.list.length,
+                    }else{
+                      return ListView.builder(
+                        itemCount: _service.list.length,
                         itemBuilder: (context,index){
                           return ListTile(
-                            title: Text(_musicService.list[index].title),
-                            subtitle: Text(_musicService.list[index].artist),
+                            title: Text(_service.list[index].title),
+                            subtitle: Text(_service.list[index].artist),
                             onTap: (){
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                   builder: (context) => 
-                                    MusicPlayerScreen(music: _musicService.list[index])),
+                                    MusicPlayerScreen(music: _service.list[index])),
                               );
                             },
                           );
                         });
                     }
-                }))
-              ],
-            ),
-          ),
-        ));
+                  }))
+            ],)
+        ),)
+    );
   }
 }
--- CursoFlutter/exemplo2_music_player/lib/screens/music_player_screen.dart
+++ CursoFlutter/exemplo2_music_player/lib/screens/music_player_screen.dart
@@ -4,41 +4,51 @@ import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
   final MusicModel music;
-  const MusicPlayerScreen({Key? key, required this.music}) : super(key: key);
+  const MusicPlayerScreen({super.key,required this.music});
 
   @override
   State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
 }
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
-  late AudioPlayer _audio;
+  late AudioPlayer _audio = AudioPlayer();
+  bool _isPlaying = false;
 
   @override
   void initState() {
     super.initState();
     _audio = AudioPlayer();
   }
-
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+    _isPlaying = !_isPlaying;
+  }
+  
   @override
   void dispose() {
     _audio.dispose();
     super.dispose();
   }
-
+  void _playPause() {
+    if (_isPlaying) {
+      _audio.pause();
+    } else {
+      _audio.play(UrlSource(widget.music.url));
+    }
+  }
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.music.title),
       ),
-      body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () async {
-              await _audio.play(widget.music.url, isLocal: false);
-            },
-            child: const Text('Play Music'),
-          ),
+      body: Center(
+        child: IconButton(
+          icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+          iconSize: 64.0,
+          onPressed: _playPause,
         ),
       ),
     );
